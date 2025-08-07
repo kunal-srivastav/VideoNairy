8f88b370facec6a1ba17d2c14d09048b805bba88
kunal srivastav <shrivastavkunal56@gmail.com>
2025-08-06 19:04:39 -0700

    To update project environment in production

--- backend/middleWares/multer.js
+++ backend/middleWares/multer.js
@@ -1,11 +1,22 @@
 const multer = require("multer");
+const path = require("path");
+const fs = require('fs');
+
+const uploadPath = path.join(__dirname, "../public/temp");
+
+// Create folder if missing
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/temp')
+      cb(null, uploadPath)
     },
     filename: function (req, file, cb) {
-      cb(null, file.originalname);
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      const ext = path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix + ext);
     }
 })
   
