d17ffafa2f6ad6c3d6af8f5608c75dfd194eb513
kunal srivastav <shrivastavkunal56@gmail.com>
2025-08-10 15:57:05 -0700

    To update project environment in production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,9 +37,9 @@ function App() {
   ];
   const hideLayout = hideLayoutPaths.includes(location.pathname);
 
-  // useEffect(() => {
-  //   // dispatch(currentUser());
-  // }, []);
+  useEffect(() => {
+    dispatch(currentUser());
+  }, []);
 
   return (
     <div className="container-fluid">
