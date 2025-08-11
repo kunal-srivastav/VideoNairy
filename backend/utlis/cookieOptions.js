40a7f2103a198045938c8c80d1a595089adcdb44
kunal srivastav <shrivastavkunal56@gmail.com>
2025-08-11 07:53:22 -0700

    To update project environment in production

--- backend/utlis/cookieOptions.js
+++ backend/utlis/cookieOptions.js
@@ -6,12 +6,10 @@ module.exports.cookieOptions = {
     httpOnly: true,
     secure: isProduction,
     sameSite: "None",
-    maxAge: 30 * 60 * 1000, // 30 minutes
   },
   refreshToken: {
     httpOnly: true,
     secure: isProduction,
     sameSite: "None",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   }
 };
