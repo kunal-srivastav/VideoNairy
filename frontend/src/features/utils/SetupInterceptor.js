84fa1a5c1902912d0c4b67500c80e263bbe16fa3
kunal srivastav <shrivastavkunal56@gmail.com>
2025-08-16 08:20:33 -0700

    Ready for production

--- frontend/src/features/utils/SetupInterceptor.js
+++ frontend/src/features/utils/SetupInterceptor.js
@@ -6,62 +6,16 @@ export const axiosInstance = axios.create({
   withCredentials: true, // cookies sent automatically
 });
 
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error) => {
-  failedQueue.forEach((prom) => {
-    if (error) prom.reject(error);
-    else prom.resolve();
-  });
-  failedQueue = [];
-};
-
 export const SetupInterceptor = (dispatch) => {
   axiosInstance.interceptors.response.use(
     (res) => res,
-    async (err) => {
-      const originalRequest = err.config;
-
-      // skip if already retried
-      if (err.response?.status === 401 && !originalRequest._retry) {
-
-        // don't try to refresh while already refreshing
-        if (isRefreshing) {
-          return new Promise((resolve, reject) => {
-            failedQueue.push({ resolve, reject });
-          })
-            .then(() => axiosInstance(originalRequest))
-            .catch((error) => Promise.reject(error));
-        }
-
-        originalRequest._retry = true;
-        isRefreshing = true;
-
-        try {
-          const result = await dispatch(refreshToken());
-
-          isRefreshing = false;
-
-          if (result.meta.requestStatus === "fulfilled") {
-            processQueue(null); // retry all queued requests
-            return axiosInstance(originalRequest);
-          } else {
-            processQueue(result.error);
-            dispatch(logoutUser());
-            window.location.href = "/users/login";
-            return Promise.reject(err);
-          }
-        } catch (refreshErr) {
-          isRefreshing = false;
-          processQueue(refreshErr);
-          dispatch(logoutUser());
-          window.location.href = "/users/login";
-          return Promise.reject(refreshErr);
-        }
+    (err) => {
+      if (err.response?.status === 401) {
+        dispatch(logoutUser());
+        window.location.href = "/users/login";
       }
-
       return Promise.reject(err);
     }
   );
 };
+
